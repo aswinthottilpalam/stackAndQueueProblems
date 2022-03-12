3572e254217330837cf2e10c3eff6a8c1e229d6f
aswinthottilpalam <aswinp264@gmail.com>
2022-03-12 16:57:00 +0530

    Creating Queue

--- Program.cs
+++ Program.cs
@@ -10,17 +10,23 @@ namespace StackAndQueueProblems
     {
         static void Main(string[] args)
         {
-            Stack stack = new Stack();
-            stack.Push(56);
-            stack.Push(30);
-            stack.Push(70);
+            //Stack stack = new Stack();
+            //stack.Push(56);
+            //stack.Push(30);
+            //stack.Push(70);
 
-            int PeakElement = stack.Peak();
-            Console.WriteLine("Peak top Element of the stack : " + PeakElement);
+            //int PeakElement = stack.Peak();
+            //Console.WriteLine("Peak top Element of the stack : " + PeakElement);
 
-            stack.Pop();
-            Console.WriteLine("After Poping the Element ");
-            stack.Display();
+            //stack.Pop();
+            //Console.WriteLine("After Poping the Element ");
+            //stack.Display();
+
+
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
 
         }
     }
---
+++ Queue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueProblems
+{
+     class Queue
+    {
+        Node Front, Rear;
+
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.Rear == null)
+            {
+                this.Rear = newNode;
+            }
+            else
+            {
+                this.Rear.Next = newNode;
+                this.Rear = newNode;
+            }
+            Console.WriteLine("Inserted into Queue: " + data);
+        }
+    }
+}
