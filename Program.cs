563bb1f583fdf031392570d72d941c9cfcd55378
aswinthottilpalam <aswinp264@gmail.com>
2022-03-12 17:03:34 +0530

    deQueue elements

--- Program.cs
+++ Program.cs
@@ -10,17 +10,17 @@ namespace StackAndQueueProblems
     {
         static void Main(string[] args)
         {
-            //Stack stack = new Stack();
-            //stack.Push(56);
-            //stack.Push(30);
-            //stack.Push(70);
+            Stack stack = new Stack();
+            stack.Push(56);
+            stack.Push(30);
+            stack.Push(70);
 
-            //int PeakElement = stack.Peak();
-            //Console.WriteLine("Peak top Element of the stack : " + PeakElement);
+            int PeakElement = stack.Peak();
+            Console.WriteLine("Peak top Element of the stack : " + PeakElement);
 
-            //stack.Pop();
-            //Console.WriteLine("After Poping the Element ");
-            //stack.Display();
+            stack.Pop();
+            Console.WriteLine("After Poping the Element ");
+            stack.Display();
 
 
             Queue queue = new Queue();
